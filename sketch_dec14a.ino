14589ae0ff784d347e72aefea7dc899de92ec53e
oscarblanq <osblasae@epsg.upv.es>
2018-12-14 09:19:27 +0100

    Primera práctica

---
+++ sketch_dec14a.ino
@@ -0,0 +1,121 @@
+#include <M5Stack.h>
+
+const int MQ_PIN = 35;
+const int MQ_DELAY = 100;
+int valores [200];
+int i = 0;
+int contador = 0;
+bool estadoGrafico = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  M5.begin();
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextSize(2);
+}
+
+void loop() {
+  
+  contador++;
+  // put your main code here, to run repeatedly:
+  if(i == 200) i = 0;
+
+  limpiarPantalla();
+  
+  //Se rellena el array con los valores obtenidos del pin
+  for(int i = 0; i < 200; i++){
+    valores[i] = analogRead(MQ_PIN);
+    //Cuando llegue al final del array
+    //vuelve a tomar otros 200 valores
+  }
+
+  //DATOS EN M5STACK
+
+  if(contador == 100){
+    if(estadoGrafico == false){
+    
+    limpiarPantalla();
+
+  M5.Lcd.print("Maxima : ");
+  int resmax = (maxima(&valores[0]));
+  M5.Lcd.println(resmax);
+  M5.Lcd.print("Valor en volts : ");
+  M5.Lcd.println(sacarVolts(resmax));
+
+  M5.Lcd.println();
+
+  M5.Lcd.print("Minima : ");
+  int resmin = (minima(&valores[0]));
+  M5.Lcd.println(resmin);
+  M5.Lcd.print("Valor en volts : ");
+  M5.Lcd.println(sacarVolts(resmin));
+  
+  M5.Lcd.println();
+
+  M5.Lcd.print("Media : ");
+  int resmed = (media(&valores[0]));
+  M5.Lcd.println(resmed);
+  M5.Lcd.print("Valor en volts : ");
+  M5.Lcd.println(sacarVolts(resmed));
+    }   
+
+    if(estadoGrafico == true){
+      M5.Lcd.println("Botón  A  Pulsado");
+    }
+  }
+  
+  if(M5.BtnA.wasPressed()){
+      limpiarPantalla();  
+     
+    if(estadoGrafico == false){
+      estadoGrafico = true;
+      Serial.println(estadoGrafico);
+      contador = 0;
+    } else {
+    estadoGrafico = false;
+    Serial.println(estadoGrafico);
+    contador = 0;
+      }
+      M5.update();
+    }
+    
+    M5.update();
+    delay(10);
+}  
+
+int maxima(int *p){
+  int resultadoMax = 0;
+  for(int j = 0; j < 200; j++){
+    if(p[j] > resultadoMax) resultadoMax = p[j];
+  }
+  return resultadoMax;
+}
+
+int minima(int *p){
+  int resultadoMin = 4095;
+  for(int j = 0; j < 200; j++){
+    if(p[j] < resultadoMin) resultadoMin = p[j];
+  }
+  return resultadoMin;
+}
+
+int media(int *p){
+  int mediatotal = 0;
+  for (int j = 0; j < 200; j++) {
+    mediatotal = mediatotal + p[i];
+  }
+  int res;
+  res = mediatotal / 200;
+  return res;
+}
+
+double sacarVolts(unsigned int var){
+    double volts = var * (5.0/4095.0);
+    return volts;
+}
+
+void limpiarPantalla(){
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0,0);  
+}
